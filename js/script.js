ca38e9db7455250e254e25b7abc20736dbaab77f
AnatoliyLastochkin <44973612+AnatoliyLastochkin@users.noreply.github.com>
2018-12-27 01:20:39 +0100

    add formscript

--- js/script.js
+++ js/script.js
@@ -5,14 +5,29 @@ var close = popup.querySelector(".close-popup");
 
 var form = popup.querySelector("form");
 
-var name = popup.querySelector("[name=fullname]");
+var nameField = popup.querySelector("[name=fullname]");
 var email = popup.querySelector("[name=email]");
 var message = popup.querySelector("[name=message]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("nameField");
+} catch (err) {
+  isStorageSupport = false;
+}
 
   feedbackLink.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
-    //name.focus(); // не работает фокус
+
+    if (storage) {
+      nameField.value = storage;
+      email.focus();
+    } else {
+      nameField.focus(); 
+    }
   });
 
   close.addEventListener("click", function (evt) {
@@ -21,12 +36,23 @@ var message = popup.querySelector("[name=message]");
   });
 
   form.addEventListener("submit", function (evt) {
-    if (!name.value || !email.value || !message.value) {
+    if (!nameField.value || !email.value || !message.value) {
       evt.preventDefault();
       console.log("Все поля обязательны для заполнения!");
     } else {
-
+      if (isStorageSupport) {
+        localStorage.setItem("nameField", nameField.value);
+      }
     }
   });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
+    }  
+  })
 
   
